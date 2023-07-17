630f756f1c693dbd4c7af65e13b6e99cb8bb341e
Neven Hsu <space.man0318@gmail.com>
2023-07-17 11:56:56 +0800

    update MidJourney prompts

--- src/prompts.py
+++ src/prompts.py
@@ -14,13 +14,11 @@ Please adhere to the structure and formatting below, and follow these guidelines
 - Do not use the words "description" or ":" in any form.
 - Write each prompt in one line without using return.
 
-Note: At the end of the prompt, you can also add a camera type if it's not a painting style, here are some examples:
-DLSR, Nikon D, Nikon D3, Canon EOS R3, Canon EOS R8
-We can also provide a lens that was used:
-Focal length 14mm, Focal length 35mm, Fisheye lens, Wide angle lens
+Note 1: If it is photography style, provide a camera type and a lens.
+Note 2: If it is japanese style, add "--niji 5" at the end of the prompt.
 
 Structure:
-[1] = [THE CONCEPT]
+[1] = some keywords I give you
 [2] = a detailed description of [1] with specific imagery details.
 [3] = a detailed description of the scene's environment.
 [4] = a detailed description of the scene's mood, feelings, and atmosphere.
@@ -30,7 +28,7 @@ Structure:
 
 Formatting: 
 Follow this prompt structure: "[1], [2], [3], [4], [5], [6], [ar]".
-Your task: Create 4 distinct prompts for each concept [1], varying in description, environment, atmosphere, and realization.
+Your task: Create 4 distinct prompts for keywords [1], varying in description, environment, atmosphere, and realization.
 - Do not describe unreal concepts as "real" or "photographic".
 - Include one realistic photographic style prompt with lens type and size.
 - Separate different prompts with two new lines.
@@ -40,16 +38,16 @@ Your task: Create 4 distinct prompts for each concept [1], varying in descriptio
 The prompts should be formatted similar to the following examples:
 
 Prompt #1
-An abstract painting capturing the essence of the inner mind, swirling colors and shapes representing thoughts and emotions, a blend of dark and vibrant hues, expressive brushstrokes, acrylic medium, --ar 1:1
+A minimalist hotel room photo with a soft double bed, a single bed, white color scheme throughout the room, minimalistic furniture and decor, spacious and airy environment, comfortable and inviting atmosphere, Photography, using a telephoto lens to highlight the arrangement of the beds and capture the depth of the room, --ar 9:16
 
 Prompt #2
-A conceptual sculpture of a figure with a cracked facade, symbolizing the fragility of the human condition, smooth polished surface contrasts with the shattered exterior, medium-sized sculpture, clay material, --ar 9:16
+A pack of velociraptors charge towards their prey in the open grass fields of Jurassic Park, sharp claws and teeth bared, a mix of fear and excitement on the faces of the tourists watching safely from a lookout tower, Illustration, hand-drawn with colored pencils and ink, --ar 1:1 --niji 5
 
 Prompt #3
-A digital illustration depicting a futuristic cityscape at dusk, towering skyscrapers reaching towards the glowing sky, neon lights illuminating the streets below, sleek and modern architecture, vibrant color palette, 3D rendering software, --ar 16:9
+A magical steam train powered by a glowing crystal, chugging through a dense forest with mystical creatures peeking from the trees, a misty ambiance surrounds the train, evoking a sense of mystery and wonder, 3D rendering with a whimsical painterly touch, --ar 3:2
 
 Prompt #4
-A black and white photograph capturing the raw emotions of a solitary figure standing on a desolate beach, strong contrast between light and shadow, grainy texture adds a sense of nostalgia, documentary-style photography, 35mm lens, --ar 4:3
+A commuter train at a busy station on a rainy day, depicting the hustle and bustle of passengers and trains as they move in and out of the station, with colorful umbrellas, reflections, and streaks of light, evoking a feeling of chaotic beauty, Photography, using a telephoto lens with f/5.6 aperture and a 70-200mm focal length, --ar 16:9
 
 ---
 
